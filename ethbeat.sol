49f0aa93a283545e071c3c9c4ccd96c9cf16c63f
Eyyüb Sari <eyyub.pangearaion@gmail.com>
2017-06-15 01:38:55 +0200

    First commit

---
+++ ethbeat.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.11;
+
+/*
+ * Ethbeat(like a heartbeat) simple smart contract acting as a proof that
+ * an account was alive at least at a certain block number. 
+*/
+contract Ethbeat {
+
+    // Store the last time(i.e. block.number) an account
+    // has called Ethbeat.beats()
+    mapping(address => uint) public ethbeats;
+    
+    event Beats(address indexed sender, uint indexed currentBlock);
+    
+    function beats() returns (address, uint) {
+        ethbeats[msg.sender] = block.number;
+        Beats(msg.sender, block.number);
+        return (msg.sender, block.number); // for local 'call' test only
+    }
+
+    // Fallback function, cannot send ether to Ethbeat smart contract
+    function() {
+        throw;
+    }
+}
